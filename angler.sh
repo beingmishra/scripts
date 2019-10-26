0070838337dec0d948ea17f1aa8e623e116f9cae
Rahul Mishra <40587889+beingmishra@users.noreply.github.com>
2019-10-26 22:46:17 +0530

    Add msm hals

--- angler.sh
+++ angler.sh
@@ -16,4 +16,8 @@ git clone https://github.com/beingmishra/kernel_huawei_angler kernel/huawei/angl
 echo -e "\n================== Cloning Vendor tree for angler ==================\n"
 git clone https://github.com/beingmishra/vendor_huawei_angler vendor/huawei/angler
 
+# MSM Hals
+echo -e "\n================== Cloning qcom msm8994 hals ==================\n"
+git clone https://android.googlesource.com/platform/hardware/qcom/msm8994 hardware/qcom/msm8994
+
 echo "Happy building :D"
